57c903b9103dd29719cdf467b2dd3d3cfd6535f1
Kyle Hilbert <104146726+KyleHilbert@users.noreply.github.com>
2022-10-06 09:49:22 -0400

    :/

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++
@@ -1,7 +0,0 @@
-const removeFromArray = function(...args) {
-    const array = args[0]
-    return array.filter(val => !args.includes(val))
-         }
-
-// Do not edit below this line
-module.exports = removeFromArray;
