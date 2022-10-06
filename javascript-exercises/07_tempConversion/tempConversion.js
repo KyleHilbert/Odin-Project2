1c7c1bf1f769dc06cff6eb8a59c8d5e3cc0d2a36
Kyle Hilbert <104146726+KyleHilbert@users.noreply.github.com>
2022-10-06 09:53:48 -0400

    Temp Conversion finished

---
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -0,0 +1,17 @@
+const ftoc = function(temp) {
+  temp = (temp-32)*5/9;
+  temp = Math.round(temp*10)/10;
+  return temp;
+};
+
+const ctof = function(temp) {
+  temp = (temp*9/5)+32;
+  temp = Math.round(temp*10)/10;
+  return temp;
+};
+
+// Do not edit below this line
+module.exports = {
+  ftoc,
+  ctof
+};
