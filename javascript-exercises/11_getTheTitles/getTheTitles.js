516686c325418fefaab9e2b101b4ac77db622ca9
Kyle Hilbert <104146726+KyleHilbert@users.noreply.github.com>
2022-10-17 10:59:46 -0400

    Finished

--- javascript-exercises/11_getTheTitles/getTheTitles.js
+++ javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+  //bookLength = books.length;
+  //const newArray = [];
 
+  //for (let i = 0; i < length; i++) {
+  //newArray.push(books[i].title);
+  //}
+
+  //return newArray;
+
+  return array.map((book) => book.title);
 };
 
 // Do not edit below this line
